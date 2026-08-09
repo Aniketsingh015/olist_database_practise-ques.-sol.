a33ba63f9482d561e7a06517a10fc1cea39ec61a
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-09 17:50:07 +0530

    Ques 3 added in fie

--- SQL/query1.sql
+++ SQL/query1.sql
@@ -15,4 +15,10 @@ LIMIT 10;
 -- Question 2 :List all orders
 -- where order_status = 'delivered'
 
-select * from olist_orders_dataset where order_status="delivered";
+select * from olist_orders_dataset
+ where order_status="delivered";
+
+-- Find all products where product_category_name = 'beleza_saude'
+
+SELECT * FROM olist_products_dataset 
+WHERE product_category_name = "beleza_saude";
