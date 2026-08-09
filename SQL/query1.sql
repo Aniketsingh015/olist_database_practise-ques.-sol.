60fbfb6baf67d04f3afd89357e0484dbb676edd6
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-09 17:58:56 +0530

    Ques 4 added in fie

--- SQL/query1.sql
+++ SQL/query1.sql
@@ -18,7 +18,10 @@ LIMIT 10;
 select * from olist_orders_dataset
  where order_status="delivered";
 
--- Find all products where product_category_name = 'beleza_saude'
+-- Ques 3:Find all products where product_category_name = 'beleza_saude'
 
 SELECT * FROM olist_products_dataset 
 WHERE product_category_name = "beleza_saude";
+
+--  Ques 4:Find all payments where payment_type = 'credit_card'
+Select * from olist_order_payments_dataset where payment_type ='credit_card';
