b99c9b6ee1f821021d89110a1af74ddb03cd36ff
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-20 12:33:06 +0530

    Ques2

--- SQL/Query2.sql
+++ SQL/Query2.sql
@@ -2,3 +2,8 @@
 
 select customer_state,count(*) from olist_customers_dataset
 group by customer_state;
+
+
+-- Count how many orders exist per order_status
+select order_status,count(*) from olist_orders_dataset
+group by order_status;
