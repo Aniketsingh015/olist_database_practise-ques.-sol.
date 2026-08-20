d3c873ad9c0cf5dd1a2c8e9f0173b70fa352feb7
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-20 12:37:02 +0530

    Ques3

--- SQL/Query2.sql
+++ SQL/Query2.sql
@@ -7,3 +7,8 @@ group by customer_state;
 -- Count how many orders exist per order_status
 select order_status,count(*) from olist_orders_dataset
 group by order_status;
+
+-- Find the average payment_value per payment_type
+
+select AVG(payment_value),payment_type from olist_order_payments_dataset
+GROUP BY payment_type;
