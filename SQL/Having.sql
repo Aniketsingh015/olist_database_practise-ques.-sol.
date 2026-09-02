e7eafc5b4cda96d23695219d0ac5f5d06b1051c0
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-02 15:33:25 +0530

    having ques 2

--- SQL/Having.sql
+++ SQL/Having.sql
@@ -1,2 +1,6 @@
 -- Leadership wants to know which states have more than 500 customers — a real market, not a negligible one.
 select count(*),customer_state from olist_customers_dataset GROUP BY customer_state having count(*)>500;
+
+-- The sellers team wants to see which sellers have sold more than 100 order items — their high-volume sellers.
+
+select count(*),seller_id from olist_order_items_dataset GROUP BY seller_id HAVING count(*)>100;
