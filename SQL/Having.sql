dc27dbab3c1dbc1a9490ee6f981cf311bbf9e5df
Aniket Singh <asaniket.singh422@gmail.com>
2026-09-02 15:35:12 +0530

    HAving ques 3 pattern 1

--- SQL/Having.sql
+++ SQL/Having.sql
@@ -4,3 +4,7 @@ select count(*),customer_state from olist_customers_dataset GROUP BY customer_st
 -- The sellers team wants to see which sellers have sold more than 100 order items — their high-volume sellers.
 
 select count(*),seller_id from olist_order_items_dataset GROUP BY seller_id HAVING count(*)>100;
+
+-- Finance wants to know which payment types have an average transaction value above 150 — identifying premium payment behavior.
+
+select avg(payment_value),payment_type from olist_order_payments_dataset GROUP BY payment_type having avg(payment_value)>150;
